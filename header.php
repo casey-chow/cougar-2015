3e476b28eac5b856d89c703078199a3f83319712
Casey Chow <casey.chow@gmail.com>
2013-02-09 15:09:23 -0500

    style content and sidebar, styling fixes

--- header.php
+++ header.php
@@ -29,7 +29,6 @@
 
   <!-- Meta -->
   <meta name="viewport" content="width=device-width,initial-scale=1.0;">
-  <meta name="description" content="<?php bloginfo('description'); ?>">
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 
   <?php if (stristr($_SERVER["HTTP_USER_AGENT"], 'facebook') !== false): ?>
